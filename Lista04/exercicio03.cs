c685afc7c4566079efca55b483d0cfdd66ae1b29
Ingrid Silva <139189235+ingridlorrane-silva@users.noreply.github.com>
2024-05-24 23:17:52 -0300

    Update exercicio03.cs

--- Lista04/exercicio03.cs
+++ Lista04/exercicio03.cs
@@ -7,19 +7,26 @@ public class Exercicio03
     {
         //Aqui eu coloco o calculo e o parametro a ser utilizado passando os valores modificados 
         //vejo se a é maior que b
+        int aux = 0;
         if (a < b)
         {
+            aux = a;
             a = b;
+            b = aux;
         }
         //Vejo se c é maior que b
         if (c > b)
         {
+            aux = b;
             b = c;
+            c = aux;
         }
         //vejo se a é realmente o maior número
         if (a < b)
         {
+            aux = a;
             a = b;
+            b = aux;
         }
     }
         public static void Pedir()
