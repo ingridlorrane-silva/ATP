da57a974e6eb7b56d7e130a6bfc07e646070c814
Ingrid Silva <139189235+ingridlorrane-silva@users.noreply.github.com>
2024-05-24 22:21:33 -0300

    Update exercicio07.cs

--- Lista04/exercicio07.cs
+++ Lista04/exercicio07.cs
@@ -1,35 +1,50 @@
 using System;
-
-class Exercicio06 { 
-
-    public static bool Verificar(int a)
+class Exercicio07
+{
+    public static void Verificar(int a)
     {
-        //aqui coloco a conferência para ver se é positivo ou não
-        if (a >= 0)
+        //Aqui eu verifico e retorno qual o conceito dele
+        if (a <= 39)
         {
-            return true;
+            Console.WriteLine("O conceito é: F");
         }
-        //se não ele é negativo
+        else if (a<=59)
+        {
+            Console.WriteLine("O conceito é: E");
+        }
+        else if(a<=69)
+        {
+            Console.WriteLine("O conceito é: D");   
+        }
+        else if (a <= 79)
+        {
+            Console.WriteLine("O conceito é: C");
+        }
+        else if (a <= 89)
+        {
+            Console.WriteLine("O conceito é: B");
+        }
+        else if (a >= 90)
+        {
+            Console.WriteLine("O conceiro é: A");
+        }
         else
         {
-            return false;
+            Console.WriteLine("A nota é inválida");
         }
-
     }
     public static void Pedir()
     {
-        //Recebo o valor e chamo o metodo 
-        int n = 0;
-        Console.WriteLine("Digite um número: ");
+        int n, med;
+        //Aqui eu peço a quantidade de alunos e a média
+        Console.WriteLine("Digite o número de alunos alunos que irá verificar a média: ");
         n = int.Parse(Console.ReadLine());
-        //Chamo o metodo
-        if (Verificar(n))
+        for (int i =0; i<n; i++)
         {
-            Console.WriteLine("O número {0} é posittivo.", n);
-        }
-        else
-        {
-            Console.WriteLine("O número {0} não é positivo.", n);
+            Console.WriteLine("Digite a média: ");
+            med = int.Parse(Console.ReadLine());
+            //Chamo o metodo
+            Verificar(med);
         }
 
     }
